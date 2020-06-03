3596e1e845984ead3314a2146d4a0147dd72d775
apurva-web <66378546+apurva-web@users.noreply.github.com>
2020-06-04 00:18:20 +0530

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u(d6hnzin%o4_(3m&o$li_%@9-yyw%t%w=%awqet@b$vl&$hq!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.pythonanywhere.com']
 
 
 # Application definition
